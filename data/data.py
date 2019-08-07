81d7acc042f49f9983d0bdb5b50c1fb9046b45f2
irynakostyshyn <i.kostyshyn@ucu.edu.ua>
2019-08-08 00:33:28 +0200

    add test, train, model

---
+++ data/data.py
@@ -0,0 +1,111 @@
+import torch
+import torchvision.transforms as transforms
+import torch.utils.data as data
+import os
+import pickle
+import numpy as np
+import nltk
+from PIL import Image
+import cv2
+import torchvision
+import argparse
+import os
+import numpy as np
+import torch
+from PIL import Image
+
+
+class Flickr8k(object):
+    def __init__(self, root, annotation, vocab, train_img,  transform):
+        self.root = root
+        self.annotation = get_captions(annotation)
+        self.transform = transform
+        self.vocab = vocab
+        self.imgs = list(set(open(train_img, 'r').read().strip().split('\n')))
+        self.captions = {}
+        print(len(self.imgs))
+        for img in self.imgs:
+            
+            self.captions[img] = self.annotation[img]
+
+    def __getitem__(self, idx):
+
+        caption = self.captions[self.imgs[idx]]
+
+        img = self.imgs[idx]
+        path = get_full_path_to_img(self.root, img)
+        vocab = self.vocab
+        
+        try:
+            image = cv2.imread(path)
+            image = Image.fromarray(image, 'RGB')
+        except Exception:
+            print("PATH", path)
+            raise
+
+        if self.transform is not None:
+            image = self.transform(image)
+        tokens = nltk.tokenize.word_tokenize(' '.join(caption).lower())
+        caption = []
+        caption.append(vocab('<start>'))
+        #caption.extend([vocab(token) for token in tokens])
+        for token in tokens:
+            
+            caption.append(vocab(token))
+        caption.append(vocab('<end>'))
+
+        target = torch.Tensor(caption)
+
+        return image, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+def get_full_path_to_img(root, img_title):
+    return root + img_title
+
+
+def collate_fn(data):
+
+    # Sort a data list by caption length (descending order).
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions = zip(*data)
+
+    images = torch.stack(images, 0)
+
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]
+
+    return images, targets, lengths
+
+
+def get_captions(annotations):
+    captions_tmp = open(annotations, 'r').read().strip().split('\n')
+
+    captions = {}
+    for row in captions_tmp:
+        title = row.split("^")[0]
+        text = row.split("^")[1]
+        if not (title in captions):
+            captions[title] = []
+
+        captions[title].append(text)
+    return captions
+
+
+def get_loader(root, ann, vocab, train_img, transform, batch_size, shuffle, num_workers):
+
+    data = Flickr8k(root=root, annotation=ann, vocab=vocab, train_img=train_img, transform=transform)
+
+    data_loader = torch.utils.data.DataLoader(dataset=data,
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              num_workers=num_workers,
+                                              collate_fn=collate_fn)
+
+    return data_loader
+
---
+++ test.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import argparse
+import pickle
+import os
+from torchvision import transforms
+from build_vocab import Vocabulary
+from model.model import EncoderCNN, DecoderRNN
+from PIL import Image
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def load_image(image_path, transform=None):
+    print(image_path)
+    image = cv2.imread(image_path)
+    image = Image.fromarray(image, 'RGB')
+
+    if transform is not None:
+        image = transform(image).unsqueeze(0)
+
+    return image
+
+
+def test(image_path, decoder_path, encoder_path, vocab_path, embed_size, hidden_size, num_layers):
+    # Image preprocessing
+    transform = transforms.Compose([
+        transforms.Resize((298, 298)),
+
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
+
+    # Load vocabulary wrapper
+    with open(vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
+
+    # Build models
+    encoder = EncoderCNN(embed_size).eval()  # eval mode (batchnorm uses moving mean/variance)
+    decoder = DecoderRNN(embed_size, hidden_size, len(vocab), num_layers)
+    encoder = encoder.to(device)
+    decoder = decoder.to(device)
+
+    # Load the trained model parameters
+    encoder.load_state_dict(torch.load(encoder_path, map_location=device))
+    decoder.load_state_dict(torch.load(decoder_path, map_location=device))
+
+    # Prepare an image
+    image = load_image(image_path, transform)
+    image_tensor = image.to(device)
+
+    # Generate an caption from the image
+    feature = encoder(image_tensor)
+    sampled_ids = decoder.sample(feature)
+    sampled_ids = sampled_ids[0].cpu().numpy()  # (1, max_seq_length) -> (max_seq_length)
+
+    # Convert word_ids to words
+    sampled_caption = []
+    for word_id in sampled_ids:
+        word = vocab.idx2word[word_id]
+        sampled_caption.append(word)
+        if word == '<end>':
+            break
+    sentence = ' '.join(sampled_caption)
+
+    # Print out the image and the generated caption
+    print(sentence)
+    return sentence
+    # image = Image.open(args.image)
+    # plt.imshow(np.asarray(image))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
+
+    parser.add_argument('--encoder_path', type=str,
+                        default='./model/historical/encoder-epoch-20-loss-0.4047403931617737.ckpt',
+                        help='path for trained encoder')
+    parser.add_argument('--decoder_path', type=str,
+                        default='./model/historical/decoder-epoch-20-loss-0.4047403931617737.ckpt',
+
+                        help='path for trained decoder')
+    parser.add_argument('--vocab_path', type=str, default='./vocab_historical2.pkl', help='path for vocabulary wrapper')
+
+    # Model parameters (should be same as paramters in train.py)
+    parser.add_argument('--embed_size', type=int, default=256, help='dimension of word embedding vectors')
+    parser.add_argument('--hidden_size', type=int, default=512, help='dimension of lstm hidden states')
+    parser.add_argument('--num_layers', type=int, default=1, help='number of layers in lstm')
+    args = parser.parse_args()
+    test(image_path=args.image,
+         decoder_path=args.decoder_path,
+         encoder_path=args.encoder_path,
+         vocab_path=args.vocab_path,
+         embed_size=args.embed_size,
+         hidden_size=args.hidden_size,
+         num_layers=args.num_layers)
